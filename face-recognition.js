1a716bc8d9c316bf2a26a078c309f0be2cbbec39
ykkim <ykkim@innosim.com>
2019-10-16 18:39:13 +0900

    jQuery 적용(서버 통신)

---
+++ face-recognition.js
@@ -0,0 +1,110 @@
+  //ID 입력
+  const video = document.getElementById('video')
+
+  // 자바스크립트는 대부분의 작업들이 비동기로 이루어짐 => 콜백 중첩 이슈 발생
+  // 따라서 Promise 패턴을 사용하여 콜백 중첩 해결
+  // 다른 모델을 불러옴 -> 얼굴인식(68개 점)
+  Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo) // startVideo 함수 실행
+
+  // 웹캠 실행 함수
+  function startVideo() {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => video.srcObject = stream,
+      err => console.error(err)
+    )
+  }
+
+  video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+
+    // 얼굴인식 데이터 출력
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height) // 캔버스를 지우고 아래서 다시 그림
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections) // 표정 출력
+
+      // 안면인식 예외처리
+      if(resizedDetections[0]==null)
+        return;
+
+      // 표정 -> "neutral","happy","sad","angry","fearful","disgusted","surprised"
+      var array = [resizedDetections[0].expressions.neutral,
+                    resizedDetections[0].expressions.happy,
+                    resizedDetections[0].expressions.sad,
+                    resizedDetections[0].expressions.angry,
+                    resizedDetections[0].expressions.fearful,
+                    resizedDetections[0].expressions.disgusted,
+                    resizedDetections[0].expressions.surprised];
+
+      var expression = ["neutral", "happy", "sad", "angry", "fearful", "disgusted", "surprised"];
+      var num;
+      var max = 0;
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+          max = array[i];
+          num = i;
+        }
+      }
+      console.log(expression[num]);
+      
+      // 졸음
+      // Point Y좌표를 비교하여 체크
+      // 왼쪽 눈(화면기준)
+      var point38 = resizedDetections[0].unshiftedLandmarks._positions[37]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+      var point42 = resizedDetections[0].unshiftedLandmarks._positions[41]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+      var point39 = resizedDetections[0].unshiftedLandmarks._positions[38]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+      var point41 = resizedDetections[0].unshiftedLandmarks._positions[40]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+      // 오른쪽 눈(화면기준)
+      var point44 = resizedDetections[0].unshiftedLandmarks._positions[43]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+      var point48 = resizedDetections[0].unshiftedLandmarks._positions[47]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+      var point45 = resizedDetections[0].unshiftedLandmarks._positions[44]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+      var point47 = resizedDetections[0].unshiftedLandmarks._positions[46]._y / resizedDetections[0].unshiftedLandmarks._imgDims._height;
+
+      console.log("1:",point42 - point38) // 범위 0.032~0.04
+      console.log("2:",point41 - point39) // 범위 0.034~0.045
+      console.log("3:",point48 - point44) // 범위 0.029~0.041
+      console.log("4:",point47 - point45) // 범위 0.035~0.045
+
+      // TEST_CODE - Text 출력
+      var ctx = canvas.getContext('2d');
+      ctx.font = '20px gothic';
+      ctx.fillStyle = "rgba(255,0,0,1)";
+      var isSleep;
+      if ((point42 - point38) < 0.038 && (point41 - point39) < 0.043 && (point48 - point44) < 0.039 && (point47 - point45) < 0.043)  // Point 사이거리
+      {
+        isSleep='sleep';
+      }
+      else 
+      {
+        isSleep='None';
+      }
+      ctx.fillText(isSleep, 50, 50);
+      console.log(isSleep)
+
+      // jQuery 서버와 통신
+      $.ajax({
+        url: '/server',
+        dataType: 'json',
+        type: 'GET',
+        data: { expression: expression[num], sleep: isSleep },
+        success: function(result) {
+            if (result) {
+              $('#get_output').html(result.result);
+            }
+       }
+    })
+    }, 100)
+    
+  })
--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-//server.js
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end('Hello World');
-// }).listen(1337, '127.0.0.1');
-// console.log('Server running at http://127.0.0.1:1337/');
-
-
-/**
+/*
  * 웹 서버에 html 파일 서비스 하기
  */
  
@@ -16,8 +7,9 @@ var fs = require('fs'); // 파일 읽기, 쓰기 등 을 할 수 있는 모듈
 var express = require('express');
 var app = express();
 
-app.use('/script.js',express.static(__dirname+"/script.js"));
+app.use('/jquery-3.4.1.min.js',express.static(__dirname+"/jquery-3.4.1.min.js"));
 app.use('/face-api.min.js',express.static(__dirname+"/face-api.min.js"));
+app.use('/face-recognition.js',express.static(__dirname+"/face-recognition.js"));
 app.use('/models',express.static(__dirname+"/models"));
 
 // 404 error message : 페이지 오류가 발생했을때,
@@ -46,18 +38,61 @@ app.get('/', function onRequest(request, response){
 http.createServer(app).listen(8888);
 console.log("Server Created...");
 
-app.get('/a',function(req,res) {
+// face-recognition.js에서 데이터 여기서 데이터 받아서 다른함수에서 루프돌리고 전송?
+var dataExpression;
+var dataSleep;
+app.get('/server',function(req,res) {
  
-    var data = req.query.data;
+    dataExpression = req.query.expression;
+    dataSleep = req.query.sleep;
 
-    console.log('GET Parameter = ' + data);
+    console.log('GET dataExpression = ' + dataExpression);
+    console.log('GET dataSleep = ' + dataSleep);
+    console.log('');
+
+    res.send({dataExpression:''});
+});
 
 
-    var result = data + ' Succese';
+// TCP클라이언트
+// Import net module.
+var net = require('net');
 
-    console.log(result);
+// This function create and return a net.Socket object to represent TCP client.
 
+// Create TCP client.
+// var client = net.connect(option, function () {
+//     console.log('Connection name : ' + connName);
+//     console.log('Connection local address : ' + client.localAddress + ":" + client.localPort);
+//     console.log('Connection remote address : ' + client.remoteAddress + ":" + client.remotePort);
+// });
+var client = net.connect({ port: 9090 });
 
-    res.send({result:result});
+//client.setTimeout(1000);
+//client.setEncoding('utf8');
 
+client.on('connect', function () {
+    console.log('Connected to server');
+
+    setInterval(function () {
+        client.write("TEST-----");
+    }, 1000);
+});
+
+// When receive server send back data.
+client.on('data', function (data) {
+    console.log('Server return data : ' + data);
+});
+
+// When connection disconnected.
+client.on('end', function () {
+    console.log('Client socket disconnect. ');
+});
+
+client.on('timeout', function () {
+    console.log('connection timeout');
+})
+
+client.on('error', function (err) {
+    console.error(JSON.stringify(err));
 });
