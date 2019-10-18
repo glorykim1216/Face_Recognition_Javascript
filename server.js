a301c2776637ec4c149eb403238bdb55bc7f7cb8
ykkim <ykkim@innosim.com>
2019-10-18 10:42:41 +0900

    sleep 적용

--- server.js
+++ server.js
@@ -1,98 +1,113 @@
 /*
  * 웹 서버에 html 파일 서비스 하기
  */
- 
+
 var http = require('http');
 var fs = require('fs'); // 파일 읽기, 쓰기 등 을 할 수 있는 모듈
 var express = require('express');
 var app = express();
 
-app.use('/jquery-3.4.1.min.js',express.static(__dirname+"/jquery-3.4.1.min.js"));
-app.use('/face-api.min.js',express.static(__dirname+"/face-api.min.js"));
-app.use('/face-recognition.js',express.static(__dirname+"/face-recognition.js"));
-app.use('/models',express.static(__dirname+"/models"));
+app.use('/jquery-3.4.1.min.js', express.static(__dirname + "/jquery-3.4.1.min.js"));
+app.use('/face-api.min.js', express.static(__dirname + "/face-api.min.js"));
+app.use('/face-recognition.js', express.static(__dirname + "/face-recognition.js"));
+app.use('/models', express.static(__dirname + "/models"));
 
 // 404 error message : 페이지 오류가 발생했을때,
-function send404Message(response){
-    response.writeHead(404,{"Content-Type":"text/plain"}); // 단순한 글자 출력
+function send404Message(response) {
+    response.writeHead(404, { "Content-Type": "text/plain" }); // 단순한 글자 출력
     response.write("404 ERROR... ");
     response.end();
 }
- 
+
 // 200 Okay : 정상적인 요청
-app.get('/', function onRequest(request, response){
- 
-    if(request.method == 'GET' && request.url == '/'){
-        response.writeHead(200,{"Content-Type":"text/html"}); // 웹페이지 출력
+app.get('/', function onRequest(request, response) {
+
+    if (request.method == 'GET' && request.url == '/') {
+        response.writeHead(200, { "Content-Type": "text/html" }); // 웹페이지 출력
         fs.createReadStream("./index.html").pipe(response); // 같은 디렉토리에 있는 index.html를 response 함
         //console.log(Test);
-        
+
     } else {
         // file이 존재 하지않을때,
         send404Message(response);
- 
+
     }
- 
+
 });
 
 http.createServer(app).listen(8888);
 console.log("Server Created...");
 
-// face-recognition.js에서 데이터 여기서 데이터 받아서 다른함수에서 루프돌리고 전송?
-var dataExpression;
-var dataSleep;
-app.get('/server',function(req,res) {
- 
-    dataExpression = req.query.expression;
-    dataSleep = req.query.sleep;
+// face-recognition.js에서 안면인식 데이터 받음
+app.get('/server', function (req, res) {
 
-    console.log('GET dataExpression = ' + dataExpression);
-    console.log('GET dataSleep = ' + dataSleep);
-    console.log('');
+    var dataExpression = req.query.expression;
+    var dataSleep = req.query.sleep;
 
-    res.send({dataExpression:''});
+    sendFaceDate(dataExpression, dataSleep);
+
+    res.send({ dataExpression: '' });
 });
 
+var timeoutObj = null;
+function sendFaceDate(expression, sleep) {
 
-// TCP클라이언트
-// Import net module.
-var net = require('net');
+    // 안면인식 실패
+    if(expression == 'null') {  
+        console.log(expression); // 서버에 데이터 전송
+        return;
+    }
 
-// This function create and return a net.Socket object to represent TCP client.
+    if(sleep == 'sleep') {
+        if (timeoutObj == null) {
+            //console.log('sleep!!!!');
+            timeoutObj = setTimeout(sleeping, 1500);
+        }
+    }
+    else if (sleep == 'None') {
+        
+        if (timeoutObj != null) {
+            clearTimeout(timeoutObj);
+            timeoutObj = null;
+        }
+    }
+    console.log(expression); // 서버에 데이터 전송
+}
 
-// Create TCP client.
-// var client = net.connect(option, function () {
-//     console.log('Connection name : ' + connName);
-//     console.log('Connection local address : ' + client.localAddress + ":" + client.localPort);
-//     console.log('Connection remote address : ' + client.remoteAddress + ":" + client.remotePort);
-// });
-var client = net.connect({ port: 9090 });
+function sleeping() {
+    console.log('TCP sleep!!!!');
+    //clearTimeout(timeoutObj);
+    timeoutObj = null;
+    //socket.write("sleep");
+}
+/////////////////////////////// 클라이언트
 
-//client.setTimeout(1000);
-//client.setEncoding('utf8');
+// var net = require('net');
 
-client.on('connect', function () {
-    console.log('Connected to server');
+// function openSocket(){
+//     var socket = net.connect({ port: 5555 });
+//     socket.setKeepAlive(true);
+//     socket.on('connect',onConnect.bind({},socket));
+//     socket.on('error',onError.bind({},socket));
+// }
 
-    setInterval(function () {
-        client.write("TEST-----");
-    }, 1000);
-});
+// var interval;
+// function onConnect(socket)
+// {
+//     console.log('TCP Socket is Open!');
+//     socket.write("TCP Socket is Open!-----");
+//     interval = setInterval(function () {
+//         socket.write("TEST-----");
+//     }, 1000);
+// }
 
-// When receive server send back data.
-client.on('data', function (data) {
-    console.log('Server return data : ' + data);
-});
+// function onError(socket){
+//     console.log('TCP Error!');
 
-// When connection disconnected.
-client.on('end', function () {
-    console.log('Client socket disconnect. ');
-});
+//     clearInterval(interval);
+//     socket.destroy();
+//     socket.unref();
 
-client.on('timeout', function () {
-    console.log('connection timeout');
-})
-
-client.on('error', function (err) {
-    console.error(JSON.stringify(err));
-});
+//     setTimeout(openSocket,1000);
+// }
+// openSocket();
